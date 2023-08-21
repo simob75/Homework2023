3a2bfe76a796ec84af8c035dcb4f8192172be416
Bruna Simonian <simob75@users.noreply.github.com>
2023-08-21 23:00:58 +0100

    Update Greeter.sol

--- Week 1/Greeter.sol
+++ Week 1/Greeter.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.7.0 < 0.9.0;
 
+contract Greeter {
+    string greeting;
+    address owner;
+    modifier onlyOwner {
+    require(msg.sender == owner, "Only the owner of this contract can set the value");
+    _;
+    }
+
+    constructor(){
+    greeting = "Hello World again";
+    owner = msg.sender;
+    }
+
+    function getValue() public view returns(string memory) {
+        return greeting;
+    }
+
+    function setValue(string memory _greeting) public onlyOwner{
+        greeting = _greeting;
+    }
+}
